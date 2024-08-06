26b2909cb239df905f65eea990583e8f8a1c66db
Jinjin Wang <jinjin.wang@colorado.edu>
2024-08-05 23:36:02 -0400

    update tests

--- src/tests/integration/TestWebAp.py
+++ src/tests/integration/TestWebAp.py
@@ -6,8 +6,8 @@ def client():
     with app.test_client() as client:
         yield client
 
-def test_gainer(client):
-    """Test the /gainer endpoint"""
+def test_hello(client):
+    """Test the /hello endpoint"""
     response = client.get('/hello')
     assert response.status_code == 200
     assert response.json == {'message': 'Hello, World!'}
--- src/tests/unit/TestWebAppGetReportData.py
+++ src/tests/unit/TestWebAppGetReportData.py
@@ -15,7 +15,6 @@ class TestWebApGetReportData(unittest.TestCase):
     '''
     @patch.object(DatabaseGateway, 'get_data', return_value=MOCKDATA)
     def test_get_gainer_data(self, mock_get_data):
-        #gateway = DatabaseGateway()
         result = get_gainer_data()
         self.assertEqual(result, self.mockdata)
         mock_get_data.assert_called_once()
